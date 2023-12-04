53d364d034402f14812c676b6e98ef1fbe29e97c
Annastiina Similä <kissamasiina@Annastiina-MacBook-Pro.local>
2023-12-04 23:20:16 +0200

    2nd commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,26 @@
+import { useRouteError } from "react-router-dom";
+import Home from "./Home";
+
+export default function Error() {
+
+    const error = useRouteError();
+    console.log(error);
+
+    return (
+
+        <>
+        <div>
+            <h1>Oh noes! Page not found.</h1>
+            <p>{error.data}</p>
+        </div>
+
+        </>
+    );
+        
+    /* Tää ois hyvä jotenkin saada:
+        <div>
+        <Link to={"/Home"}>Etusivu</Link>
+        <Outlet />
+        </div> 
+        */
+}
